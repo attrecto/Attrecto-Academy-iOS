20a1a0264a3b9ade477d08f0a4c80eb183aef8a1
Papp Balazs <papp.balazs1@gmail.com>
2019-09-24 07:40:59 +0200

    init 2019_02

---
+++ iOS_Academy_2019_2/iOS_Academy_2019_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Academy_2019_2
+//
+//  Created by Papp Balázs on 2019. 09. 24..
+//  Copyright © 2019. Attrecto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
